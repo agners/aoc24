bbc02256c76a6ef9b951b50afca5900c9996941c
Stefan Agner <stefan@agner.ch>
2024-12-08 10:02:29 +0100

    Day 8 part 1 and 2

---
+++ day8/day8-part2.py
@@ -0,0 +1,49 @@
+"""Advent of Code 2024 Day 8"""
+
+import time
+
+def check_bounds(resonance: tuple[int, int], x_size: int, y_size: int) -> bool:
+    return resonance[0] >= 0 and resonance[0] < x_size and resonance[1] >= 0 and resonance[1] < y_size
+
+start = time.monotonic()
+with open("input.txt") as f:
+    total = 0
+    antennas: dict[list[tuple[int, int]]] = {}
+
+    lines = f.readlines()
+    x = 0
+    y = 0
+    
+    for line in lines:
+        x = 0
+        for c in line:
+            if c.isdigit() or c.isalpha():
+                if c in antennas:
+                    antennas[c].append((x, y))
+                else:
+                    antennas[c] = [(x, y)]
+            x += 1
+        y += 1
+    
+    x_size = x
+    y_size = y
+    print("Size", x_size, y_size)
+
+    resonance_nodes = set()
+
+    for k, v in antennas.items():
+        for v1 in v:
+            for v2 in antennas[k]:
+                if v1 == v2:
+                    continue
+                resonance_nodes.add(v1)
+                resonance_nodes.add(v2)
+                diff = (v2[0] - v1[0], v2[1] - v1[1])
+                r1 = (v1[0] - diff[0], v1[1] - diff[1])
+                while check_bounds(r1, x_size, y_size):
+                    resonance_nodes.add(r1)
+                    r1 = (r1[0] - diff[0], r1[1] - diff[1])
+
+    print(len(resonance_nodes))
+
+print("Time:", time.monotonic() - start)
